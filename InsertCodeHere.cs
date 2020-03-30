140f2c3f19bbc47a6fa6cad37ee1a3f513bcd1a3
Madeleine Glindorf <mglindorf@gmail.com>
2020-03-30 13:26:56 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Sandbox
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            
+            string title = "Mad Max";
+            int year = 2020;
+            Console.WriteLine($"Movie name: {title}\nYear of release: {year}");
+            
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
